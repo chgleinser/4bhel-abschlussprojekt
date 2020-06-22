e024a4be4eb0415e19510dc37df9d63345b1e101
GooGooJoob <andreas.gleinser@aon.at>
2020-06-22 19:52:54 +0200

    pyCl fast fertig für CLI gebrauch

---
+++ pyCl.py
@@ -0,0 +1,72 @@
+#------------------------------------------------------------------------------
+#   Project:    Chatprogram (Clientside)
+#   File:       pyCl.py
+#   Author:     GleAn, GleCh
+#   Date:       22.06.2020
+#   Version:    v0.1
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+# Log:
+#   v0.1:   - created file "pyClient.py"
+#           - added comments
+#           - created main routine
+#
+# Description:
+#   This program realises a chatprogram with the help of the functionality of
+#   sockets. The Server is the "chatroom" to which the clients connect to. The
+#   clients can communicate with each other over the chatroom. This program is
+#   the clientside of the project. This program should be ready for use in a
+#   CLI and a GUI-Framework like tkinter.
+#------------------------------------------------------------------------------
+
+#----- imports ----------------------------------------------------------------
+import socket, sys, select
+
+#----- variables --------------------------------------------------------------
+cl = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+address = sys.argv[1]
+port = 2222
+
+#----- subroutines ------------------------------------------------------------
+
+# Function: nameHandler
+# ---------------------
+# Awaits input for a name and sends it to the server
+#
+# return:   name
+def nameHandler():
+    name = input("Please input name for chatroom: ")
+    cl.send(bytes(name, 'UTF-8'))
+    return name
+
+# Function: receiveMsg
+# --------------------
+# handles messages being received
+#
+# return:   nothing
+def receiveMsg():
+    msgRecv = cl.recv(1024).decode('UTF-8')
+    print(msgRecv)
+
+#----- main routine -----------------------------------------------------------
+def main():
+
+    #----- initialization -----------------------------------------------------
+    cl.connect((address, port))
+    name = nameHandler()
+
+    #----- main loop ----------------------------------------------------------
+    while True:
+        receiveMsg()
+        msgSend = input("<%s>" % name)
+        cl.send(bytes(msgSend, 'UTF-8'))
+        if msgSend == 'bye':
+            print("<Sucessfully Disconnected!>")
+            break
+        receiveMsg()
+
+if __name__ == '__main__':
+    main()
+
+# [] END OF FILE
