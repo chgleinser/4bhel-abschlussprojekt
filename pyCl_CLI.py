1e56e6d0511e62684bce76431f71dab194bf32a2
GleAndi <el.andgleinser@gmail.com>
2020-07-03 08:32:22 +0200

    created file for testing cli version

--- pyCl_CLI.py
+++ pyCl_CLI.py
@@ -1,6 +1,6 @@
 #------------------------------------------------------------------------------
 #   Project:    Chatprogram (Clientside)
-#   File:       pyCl.py
+#   File:       pyCl_CLI.py
 #   Author:     GleAn, GleCh
 #   Date:       22.06.2020
 #   Version:    v0.2
@@ -8,15 +8,9 @@
 
 #------------------------------------------------------------------------------
 # Log:
-#   v0.1:   - created file "pyCl.py"
+#   v0.1:   - copied "pyCl.py" and renamed to "pyCl_CLI.py"
 #           - added comments
 #           - created main routine
-#
-#   v0.2:   - added gui through tkinter, because it's easier to program the
-#             client, than with a CLI Version
-#
-#   v0.3:   - finished program. note to self: don't try to program the client
-#             for CLI-use. NO REALLY! it's gonna make it more complicated
 #
 # Description:
 #   This program realises a chatprogram with the help of the functionality of
@@ -34,27 +28,6 @@ import tkinter as tk
 cl = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 address = sys.argv[1]
 port = int(sys.argv[2])
-
-#----- tkinter variables ------------------------------------------------------
-window = tk.Tk()
-chatroom = tk.Text(window)
-msgField = tk.Entry(window)
-
-#----- subroutines ------------------------------------------------------------
-
-# Function: sendMsg
-# -----------------
-# handles message for sending to server
-#
-# entry:    tkinter Entry for reading the Text
-#
-# return:   nothing
-def sendMsg():
-    msg = msgField.get()
-    cl.send(bytes(msg, 'UTF-8'))
-    msgField.delete(0, 'end')
-    if msg == "bye":
-        window.destroy()
 
 #----- classes ----------------------------------------------------------------
 
@@ -81,24 +54,40 @@ class recvThread(threading.Thread):
         while True:
             try:
                 msg = cl.recv(1024).decode('UTF-8')
-                chatroom.insert(tk.END, msg + '\n')
+                print(msg)
             except OSError:
                 break
+
+#----- subroutines ------------------------------------------------------------
+
+# Function: namHandler
+# -----------------
+# handles name for sending to server
+#
+# return:   name
+def nameHandler():
+    name = input()
+    cl.send(bytes(name, 'UTF-8'))
+    msg = cl.recv(1024).decode('UTF-8')
+    print(msg)
+    return name
 
 #----- main routine -----------------------------------------------------------
 def main():
 
     #----- initialization -----------------------------------------------------
     cl.connect((address, port))
-    sendButton = tk.Button(window, text="Send", command=sendMsg)
-    chatroom.pack()
-    msgField.pack()
-    sendButton.pack()
+    name = nameHandler()
+    newThread = recvThread()
+    newThread.start()
 
     #----- main loop ----------------------------------------------------------
-    newThread = recvThread()
-    window.after(0, newThread.start())
-    window.mainloop()
+    while True:
+        try:
+            data = input("<%s>" % name)
+            cl.send(bytes(data, 'UTF-8'))
+            if msg == "bye":
+                break
 
     #----- closing connection -------------------------------------------------
     cl.close()
